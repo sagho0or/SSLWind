670f0873618c58a59c87e41127d64c105d141db5
unknown <saghar.fadaei@gmail.com>
2025-03-06 18:55:18 +0000

    fix CORS error on mock API

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,29 @@ import bundleAnalyzer from '@next/bundle-analyzer';
 const nextConfig = {
   output: 'standalone',
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 }
+
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
